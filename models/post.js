eb10b0b1d25ec3d2131032842decb01062bce602
Dongjin <ehdwls1638@gmail.com>
2023-12-06 01:22:03 +0900

    init : 9.5

--- models/post.js
+++ models/post.js
@@ -1,43 +1,32 @@
 const Sequelize = require('sequelize');
 
 class Post extends Sequelize.Model {
-    static initialize(sequelize) {
-        Post.init({
-            author: {
-                type: Sequelize.STRING(20),
-                allowNull: false,
-            },
-            title: {
-                type: Sequelize.STRING(100),
-                allowNull: false,
-            },
-            content: {
-                type: Sequelize.TEXT,
-                allowNull: false,
-            },
-            createdAt: {
-                type: Sequelize.DATE,
-                allowNull: false,
-                defaultValue: Sequelize.NOW,
-            },
-            updatedAt: {
-                type: Sequelize.DATE,
-                allowNull: true,
-            }
-        }, {
-            sequelize,
-            timestamps: false,
-            underscored: false,
-            modelName: 'Post',
-            tableName: 'posts',
-            paranoid: false,
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
-        })
-    }
-    static associate(db) {
-        db.Post.belongsTo(db.Member, { foreignKey: 'author', targetKey: 'id'});
-    }
+  static initiate(sequelize) {
+    Post.init({
+      content: {
+        type: Sequelize.STRING(140),
+        allowNull: false,
+      },
+      img: {
+        type: Sequelize.STRING(200),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'Post',
+      tableName: 'posts',
+      paranoid: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    });
+  }
+  
+  static associate(db) {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, {through: 'PostHashtag'});
+  }
 }
 
 module.exports = Post;
